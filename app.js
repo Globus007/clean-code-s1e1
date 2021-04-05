bfe8444155b15406cca607f89f5cc9af0eb873f7
Gleb Stashinsky <tragy@list.ru>
2021-04-05 16:56:14 +0300

    refactor: rename classes for BEM notation according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="list__input input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="btn edit";
+    editButton.className="btn btn--edit";
 
-    deleteButton.className="btn delete";
+    deleteButton.className="btn btn--delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="btn__img";
     deleteButton.appendChild(deleteButtonImg);
@@ -89,8 +89,8 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var editBtn=listItem.querySelector(".btn--edit");
+    var containsClass=listItem.classList.contains("list--edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
 
@@ -104,7 +104,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("list--edit-mode");
 };
 
 
@@ -161,8 +161,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.btn--edit");
+    var deleteButton=taskListItem.querySelector("button.btn--delete");
 
 
     //Bind editTask to edit button.
